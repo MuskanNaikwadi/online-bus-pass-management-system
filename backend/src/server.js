581d6af6ae8b9a9083a7dfb630872d48953bd9cf
MuskanNaikwadi <muskannaikwadi021@gmail.com>
2026-07-18 20:15:03 +0530

    Update CORS for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "http://localhost:3001"
+    "http://localhost:3001",
+    process.env.FRONTEND_URL
   ],
   credentials: true
 }));
